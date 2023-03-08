0fd635925f952cf1681dbb17d7bdbf7147b93bce
zeufi <zeufackjojo@yahoo.fr>
2023-03-08 16:42:15 +0100

    Adding browser and os metrics

--- resources/tr_firefox_script.sh
+++ resources/tr_firefox_script.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/bash#!/usr/bin/bash
 
 ###################################################################################################
 #     This is extracting the values for each test suite description.                              #
@@ -7,16 +7,16 @@
 #     <testng-results ignored="0" total="23" passed="22" failed="1" skipped="0">                  #
 ###################################################################################################
 
-TEST_RESULTS_LOCATION="${1:-/home/runner/work/selenium-webdriver-java-course/selenium-webdriver-java-course/webdriver_java/target/surefire-reports}"
+TEST_RESULTS_LOCATION="${1:-/home/runner/work/ebanking/ebanking/target/surefire-reports}"
 # shellcheck disable=SC2002
 TEST_RESULTS_STRING=$(cat "${TEST_RESULTS_LOCATION}/testng-results.xml" | grep "<testng-results")
 
 cat <<EOF | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
 github_actions_ignored_tests{action_id="${GITHUB_RUN_NUMBER}", commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $2 }')
-github_actions_total_tests{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $4 }')
-github_actions_passed_tests{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $6 }')
-github_actions_failed_tests{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $8 }')
-github_actions_skipped_tests{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')
+github_actions_total_tests{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}", os="${RUNNER_OS}", browser="${GITHUB_JOB}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $4 }')
+github_actions_passed_tests{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}", os="${RUNNER_OS}", browser="${GITHUB_JOB}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $6 }')
+github_actions_failed_tests{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}", os="${RUNNER_OS}", browser="${GITHUB_JOB}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $8 }')
+github_actions_skipped_tests{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}", os="${RUNNER_OS}", browser="${GITHUB_JOB}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')
 EOF
 
 echo " gha.maventest.ignored=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $2 }')" >> $BUILDEVENT_FILE
