7eee0f7f7ad07bdcc0cc5f249734fe95a824ef3e
zeufi <zeufackjojo@yahoo.fr>
2023-04-04 12:56:22 +0200

    Fix missing Chrome collect data.

--- resources/tr_firefox_script.sh
+++ resources/tr_firefox_script.sh
@@ -12,16 +12,16 @@ TEST_RESULTS_LOCATION="${1:-/home/runner/work/ebanking/ebanking/target/surefire-
 TEST_RESULTS_STRING=$(cat "${TEST_RESULTS_LOCATION}/testng-results.xml" | grep "<testng-results")
 
 cat <<EOF | curl --data-binary @- "${PUSHGATEWAY_URL}"/metrics/job/github_actions
-github_actions_ignored_tests_firefox{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}",actor="${GITHUB_ACTOR}",branch="${GITHUB_REF}",os="${RUNNER_OS}",browser="${GITHUB_JOB}"} $(echo "${TEST_RESULTS_STRING}" | awk -F'"' '/ignored/ {print $2}')
-github_actions_total_tests_firefox{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}",actor="${GITHUB_ACTOR}",branch="${GITHUB_REF}",os="${RUNNER_OS}",browser="${GITHUB_JOB}"} $(echo "${TEST_RESULTS_STRING}" | awk -F'"' '/total/ {print $2}')
-github_actions_passed_tests_firefox{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}",actor="${GITHUB_ACTOR}",branch="${GITHUB_REF}",os="${RUNNER_OS}",browser="${GITHUB_JOB}"} $(echo "${TEST_RESULTS_STRING}" | awk -F'"' '/passed/ {print $2}')
-github_actions_failed_tests_firefox{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}",actor="${GITHUB_ACTOR}",branch="${GITHUB_REF}",os="${RUNNER_OS}",browser="${GITHUB_JOB}"} $(echo "${TEST_RESULTS_STRING}" | awk -F'"' '/failed/ {print $2}')
-github_actions_skipped_tests_firefox{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}",actor="${GITHUB_ACTOR}",branch="${GITHUB_REF}",os="${RUNNER_OS}",browser="${GITHUB_JOB}"} $(echo "${TEST_RESULTS_STRING}" | awk -F'"' '/skipped/ {print $2}')
+github_actions_ignored_tests_firefox{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}",actor="${GITHUB_ACTOR}",branch="${GITHUB_REF}",os="${RUNNER_OS}",browser="${GITHUB_JOB}"} $(echo "${TEST_RESULTS_STRING}" | awk -F'"' '{ print $2 }')
+github_actions_total_tests_firefox{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}",actor="${GITHUB_ACTOR}",branch="${GITHUB_REF}",os="${RUNNER_OS}",browser="${GITHUB_JOB}"} $(echo "${TEST_RESULTS_STRING}" | awk -F'"' '{ print $4 }')
+github_actions_passed_tests_firefox{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}",actor="${GITHUB_ACTOR}",branch="${GITHUB_REF}",os="${RUNNER_OS}",browser="${GITHUB_JOB}"} $(echo "${TEST_RESULTS_STRING}" | awk -F'"' '{ print $6 }')
+github_actions_failed_tests_firefox{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}",actor="${GITHUB_ACTOR}",branch="${GITHUB_REF}",os="${RUNNER_OS}",browser="${GITHUB_JOB}"} $(echo "${TEST_RESULTS_STRING}" | awk -F'"' '{ print $8 }')
+github_actions_skipped_tests_firefox{action_id="${GITHUB_RUN_NUMBER}",commit="${GITHUB_SHA}",actor="${GITHUB_ACTOR}",branch="${GITHUB_REF}",os="${RUNNER_OS}",browser="${GITHUB_JOB}"} $(echo "${TEST_RESULTS_STRING}" | awk -F'"' '{ print $10 }')
 EOF
 
 # shellcheck disable=SC2129
-echo "gha.maventest.ignored=$(echo "${TEST_RESULTS_STRING}" | awk -F'"' '/ignored/ {print $2}')" >> "$BUILDEVENT_FILE"
-echo "gha.maventest.total=$(echo "${TEST_RESULTS_STRING}" | awk -F'"' '/total/ {print $2}')" >> "$BUILDEVENT_FILE"
-echo "gha.maventest.passed=$(echo "${TEST_RESULTS_STRING}" | awk -F'"' '/passed/ {print $2}')" >> "$BUILDEVENT_FILE"
-echo "gha.maventest.failed=$(echo "${TEST_RESULTS_STRING}" | awk -F'"' '/failed/ {print $2}')" >> "$BUILDEVENT_FILE"
-echo "gha.maventest.skipped=$(echo "${TEST_RESULTS_STRING}" | awk -F'"' '/skipped/ {print $2}')" >> "$BUILDEVENT_FILE"
+echo " gha.maventest.ignored=$(echo "${TEST_RESULTS_STRING}" | awk -F'"' '{ print $2 }')" >> "$BUILDEVENT_FILE"
+echo " gha.maventest.total=$(echo "${TEST_RESULTS_STRING}" | awk -F'"' '{ print $4 }')" >> "$BUILDEVENT_FILE"
+echo " gha.maventest.passed=$(echo "${TEST_RESULTS_STRING}" | awk -F'"' '{ print $6 }')" >> "$BUILDEVENT_FILE"
+echo " gha.maventest.failed=$(echo "${TEST_RESULTS_STRING}" | awk -F'"' '{ print $8 }')" >> "$BUILDEVENT_FILE"
+echo " gha.maventest.skipped=$(echo "${TEST_RESULTS_STRING}" | awk -F'"' '{ print $10 }')" >> "$BUILDEVENT_FILE"
